9297adf4c17d2d3b05e4d425b07590afa01218f2
Your Name <you@example.com>
2022-03-13 15:33:44 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -54,7 +54,7 @@ typedef struct {
 
 
 Persona* crearPersona(char nombre[], char rut[], int edad) {
-   Persona *arreglo = NULL;
+   Persona *arreglo = (Vector *) malloc (sizeof(Vector));
    strcpy(arreglo->nombre, nombre);
    strcpy(arreglo->rut, rut);
    arreglo->edad = edad;
